1bc48876656a4dd983fa60d1e1b7f24626f42cc9
development <deepsaha@natitsolved.com>
2018-08-28 19:14:45 +0530

    fourth

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello world third');
+ res.send('God is great. Thank you for being with us');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
